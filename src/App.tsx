c6a93986607ae6a1cb7d236b0c7a0ad2117bd962
talkiedoorkey <89324532+talkiedoorkey@users.noreply.github.com>
2022-02-08 21:19:10 +0900

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { Button, ChakraProvider } from "@chakra-ui/react";
+import theme from "./theme/theme";
+
+export default function App() {
+  return (
+    <div className="App">
+      <ChakraProvider theme={theme}>
+        <Button colorScheme="teal">ボタン</Button>
+        <p>aaaaaaaaaaaaa</p>
+      </ChakraProvider>
+    </div>
+  );
+}
