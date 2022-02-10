c31574dd1a0e67e0325788487c8a587b5adcd68f
talkiedoorkey <89324532+talkiedoorkey@users.noreply.github.com>
2022-02-11 08:32:14 +0900

    Finish unit 67 routing

--- src/router/HomeRoutes.tsx
+++ src/router/HomeRoutes.tsx
@@ -1,6 +1,7 @@
 import { Home } from "../components/pages/Home";
 import { UserManagement } from "../components/pages/UserManagement";
 import { Setting } from "../components/pages/Setting";
+import { Page404 } from "../components/pages/Page404";
 
 export const HomeRoutes = [
   {
@@ -17,5 +18,10 @@ export const HomeRoutes = [
     path: "/setting",
     exact: false,
     children: <Setting />
+  },
+  {
+    path: "*",
+    exact: false,
+    children: <Page404 />
   }
 ];
