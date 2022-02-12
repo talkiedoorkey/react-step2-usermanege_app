b1187a4430d27eb6d197194f8b65045b055f0523
talkiedoorkey <89324532+talkiedoorkey@users.noreply.github.com>
2022-02-12 21:59:53 +0900

    umconplete unit75

--- src/components/pages/UserManagement.tsx
+++ src/components/pages/UserManagement.tsx
@@ -1,5 +1,16 @@
+import { Wrap, WrapItem } from "@chakra-ui/react";
 import { memo, VFC } from "react";
 
 export const UserManagement: VFC = memo(() => {
-  return <p>ユーザー管理ページです</p>;
+  return (
+    <Wrap>
+      {[...Array(10)].map(() => (
+        <WrapItem>
+          <div
+            style={{ width: "100px", height: "100px", backgroundColor: "teal" }}
+          />
+        </WrapItem>
+      ))}
+    </Wrap>
+  );
 });
